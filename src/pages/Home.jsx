91594f24795bb5d29bdcf25e38f6e27515e2417d
sanjayr-12 <sanjayr.bit22@gmail.com>
2024-12-14 18:08:42 +0530

    updated skeleton

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,14 +7,17 @@ const Home = () => {
   const handleClick = () => {
     navigate("/subscribe");
   };
+  const handleImageLoad = () => {
+    setLoaded(true);
+  };
   return (
     <div className="hero bg-base-200 min-h-screen">
       <div className="hero-content flex-col lg:flex-row">
-        {!loaded && <div className="skeleton h-32 w-32"></div>}
+        {!loaded && <div className="skeleton w-[14rem] h-72"></div>}
         <img
           src="https://i.ibb.co/xMXzvBg/peter.jpg"
           className="max-w-sm rounded-lg shadow-2xl max-h-72"
-          onLoad={() => setLoaded(true)}
+          onLoad={handleImageLoad}
           style={loaded ? {} : { display: "none" }}
         />
         <div className="flex flex-col justify-center text-center w-full">
