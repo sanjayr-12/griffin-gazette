d9f4f121f95e2b391342d3abd332606c85268a99
sanjayr-12 <sanjayr.bit22@gmail.com>
2025-01-14 11:42:37 +0530

    updated

--- src/components/Thank.tsx
+++ src/components/Thank.tsx
@@ -4,16 +4,9 @@ const Thank = () => {
       <div className="hero-content flex-col lg:flex-row-reverse">
         <div className="text-center lg:text-left">
           <h1 className="text-4xl font-bold text-center">Thank You!</h1>
-          {/* <p className="py-6 text-lg text-center">
+          <p className="py-6 text-lg text-center">
             Alright, that’s it—you’ve made it! You’re all set now. You’ll start
             receiving the topics daily at 11:10 PM. Sit back, relax, and enjoy!
-          </p> */}
-          <p className="py-6 text-lg text-center">
-            Hey there, pals! Thanks for subscribing, but uh... bad news. Turns
-            out, I'm totally broke—like, can't-even-buy-a-donut broke. So, no
-            topics for now. Yep, I'm taking a little break. Don’t worry though,
-            I’ll let you know when I’m back and ready to rock your inbox again!
-            Until then, keep being awesome... or not, I dunno, heh heh!
           </p>
         </div>
       </div>
